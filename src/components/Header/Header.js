9b132ba259bcb7765d17c036497d462d63659958
shakhawat-hridoy <shakhawat.hridoy56@gmail.com>
2022-04-04 20:35:33 +0600

    primary route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-           <h1>Head</h1>
+           <nav >
+               {/* <Link className='mr-4' to="/">Home</Link> */}
+               <Link className='mr-4' to="/home">Home</Link>
+               <Link className='mr-4' to="/reviews">Reviews</Link>
+               <Link className='mr-4' to="/dashboard">Dashboard</Link>
+               <Link className='mr-4' to="/blogs">Blogs</Link>
+               <Link className='mr-4' to="/about">About</Link>
+           </nav>
         </div>
     );
 };
